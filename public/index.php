23289f688e70d4b35a5daa4bddd47ad393d0c4f6
Yahya mallak <Yahya10mallak@gmail.com>
2022-09-12 19:27:28 +0100

    add MVC public

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<?php 
+
+namespace Google;
+use Google\LIB\FrontController;
+
+if(!defined('DS')) {
+    define('DS', DIRECTORY_SEPARATOR);
+}
+
+require_once '..' . DS . 'app' . DS . 'config.php';
+require_once APP_PATH . DS . 'lib' . DS . 'autoload.php';
+
+$frontController = new FrontController();
+$frontController->dispatch();
